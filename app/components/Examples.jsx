00965b2d7ac715f373a9241d4c34143ce518dffa
hanys92 <hanusek.a@gmail.com>
2017-04-01 19:50:25 +0200

    Style examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,29 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 
 var Examples = props => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Bla Bla Bla</p>
+			<h1 className="text-centered">Examples</h1>
+			<p>Here are few locations to try out:</p>
+			<ol>
+				<li>
+					<Link to="/?location=Boston">Boston, USA</Link>
+				</li>
+				<li>
+					<Link to="/?location=Warsaw">Warsaw, Poland</Link>
+				</li>
+				<li>
+					<Link to="/?location=Berlin">Berlin, Germany</Link>
+				</li>
+				<li>
+					<Link to="/?location=London">London, UK</Link>
+				</li>
+				<li>
+					<Link to="/?location=Paris">Paris, France</Link>
+				</li>
+			</ol>
 		</div>
 	)};
 
